fbd6df49b501e2d3911eebbf707d6b3e39d4a330
Acer <Acer@DESKTOP-6MU0EJD>
2025-05-09 16:58:47 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace AndroidDataViewer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Dispose method
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.dgvContacts = new System.Windows.Forms.DataGridView();
+            this.dgvSMS = new System.Windows.Forms.DataGridView();
+            this.dgvCalls = new System.Windows.Forms.DataGridView();
+            this.txtDeviceInfo = new System.Windows.Forms.TextBox();
+
+            this.BtnLoadContacts = new System.Windows.Forms.Button();
+            this.BtnSaveContactsToDB = new System.Windows.Forms.Button();
+            this.BtnLoadSMS = new System.Windows.Forms.Button();
+            this.BtnSaveSMSToDB = new System.Windows.Forms.Button();
+            this.BtnLoadCalls = new System.Windows.Forms.Button();
+            this.BtnSaveCallsToDB = new System.Windows.Forms.Button();
+            this.BtnLoadDeviceInfo = new System.Windows.Forms.Button();
+            this.BtnSaveDeviceInfoToDB = new System.Windows.Forms.Button();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSMS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCalls)).BeginInit();
+
+            this.SuspendLayout();
+
+            // dgvContacts
+            this.dgvContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContacts.Location = new System.Drawing.Point(12, 12);
+            this.dgvContacts.Name = "dgvContacts";
+            this.dgvContacts.Size = new System.Drawing.Size(500, 150);
+
+            // dgvSMS
+            this.dgvSMS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSMS.Location = new System.Drawing.Point(12, 180);
+            this.dgvSMS.Name = "dgvSMS";
+            this.dgvSMS.Size = new System.Drawing.Size(500, 150);
+
+            // dgvCalls
+            this.dgvCalls.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCalls.Location = new System.Drawing.Point(12, 350);
+            this.dgvCalls.Name = "dgvCalls";
+            this.dgvCalls.Size = new System.Drawing.Size(500, 150);
+
+            // txtDeviceInfo
+            this.txtDeviceInfo.Location = new System.Drawing.Point(12, 520);
+            this.txtDeviceInfo.Multiline = true;
+            this.txtDeviceInfo.Name = "txtDeviceInfo";
+            this.txtDeviceInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDeviceInfo.Size = new System.Drawing.Size(500, 100);
+
+            // BtnLoadContacts
+            this.BtnLoadContacts.Location = new System.Drawing.Point(530, 12);
+            this.BtnLoadContacts.Name = "BtnLoadContacts";
+            this.BtnLoadContacts.Size = new System.Drawing.Size(150, 30);
+            this.BtnLoadContacts.Text = "Load Contacts";
+            this.BtnLoadContacts.Click += new System.EventHandler(this.BtnLoadContacts_Click);
+
+            // BtnSaveContactsToDB
+            this.BtnSaveContactsToDB.Location = new System.Drawing.Point(530, 48);
+            this.BtnSaveContactsToDB.Name = "BtnSaveContactsToDB";
+            this.BtnSaveContactsToDB.Size = new System.Drawing.Size(150, 30);
+            this.BtnSaveContactsToDB.Text = "Save Contacts to DB";
+            this.BtnSaveContactsToDB.Click += new System.EventHandler(this.BtnSaveContactsToDB_Click);
+
+            // BtnLoadSMS
+            this.BtnLoadSMS.Location = new System.Drawing.Point(530, 180);
+            this.BtnLoadSMS.Name = "BtnLoadSMS";
+            this.BtnLoadSMS.Size = new System.Drawing.Size(150, 30);
+            this.BtnLoadSMS.Text = "Load SMS";
+            this.BtnLoadSMS.Click += new System.EventHandler(this.BtnLoadSMS_Click);
+
+            // BtnSaveSMSToDB
+            this.BtnSaveSMSToDB.Location = new System.Drawing.Point(530, 216);
+            this.BtnSaveSMSToDB.Name = "BtnSaveSMSToDB";
+            this.BtnSaveSMSToDB.Size = new System.Drawing.Size(150, 30);
+            this.BtnSaveSMSToDB.Text = "Save SMS to DB";
+            this.BtnSaveSMSToDB.Click += new System.EventHandler(this.BtnSaveSMSToDB_Click);
+
+            // BtnLoadCalls
+            this.BtnLoadCalls.Location = new System.Drawing.Point(530, 350);
+            this.BtnLoadCalls.Name = "BtnLoadCalls";
+            this.BtnLoadCalls.Size = new System.Drawing.Size(150, 30);
+            this.BtnLoadCalls.Text = "Load Calls";
+            this.BtnLoadCalls.Click += new System.EventHandler(this.BtnLoadCalls_Click);
+
+            // BtnSaveCallsToDB
+            this.BtnSaveCallsToDB.Location = new System.Drawing.Point(530, 386);
+            this.BtnSaveCallsToDB.Name = "BtnSaveCallsToDB";
+            this.BtnSaveCallsToDB.Size = new System.Drawing.Size(150, 30);
+            this.BtnSaveCallsToDB.Text = "Save Calls to DB";
+            this.BtnSaveCallsToDB.Click += new System.EventHandler(this.BtnSaveCallsToDB_Click);
+
+            // BtnLoadDeviceInfo
+            this.BtnLoadDeviceInfo.Location = new System.Drawing.Point(530, 520);
+            this.BtnLoadDeviceInfo.Name = "BtnLoadDeviceInfo";
+            this.BtnLoadDeviceInfo.Size = new System.Drawing.Size(150, 30);
+            this.BtnLoadDeviceInfo.Text = "Load Device Info";
+            this.BtnLoadDeviceInfo.Click += new System.EventHandler(this.BtnLoadDeviceInfo_Click);
+
+            // BtnSaveDeviceInfoToDB
+            this.BtnSaveDeviceInfoToDB.Location = new System.Drawing.Point(530, 556);
+            this.BtnSaveDeviceInfoToDB.Name = "BtnSaveDeviceInfoToDB";
+            this.BtnSaveDeviceInfoToDB.Size = new System.Drawing.Size(150, 30);
+            this.BtnSaveDeviceInfoToDB.Text = "Save Device Info to DB";
+            this.BtnSaveDeviceInfoToDB.Click += new System.EventHandler(this.BtnSaveDeviceInfoToDB_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(700, 640);
+            this.Controls.Add(this.dgvContacts);
+            this.Controls.Add(this.dgvSMS);
+            this.Controls.Add(this.dgvCalls);
+            this.Controls.Add(this.txtDeviceInfo);
+            this.Controls.Add(this.BtnLoadContacts);
+            this.Controls.Add(this.BtnSaveContactsToDB);
+            this.Controls.Add(this.BtnLoadSMS);
+            this.Controls.Add(this.BtnSaveSMSToDB);
+            this.Controls.Add(this.BtnLoadCalls);
+            this.Controls.Add(this.BtnSaveCallsToDB);
+            this.Controls.Add(this.BtnLoadDeviceInfo);
+            this.Controls.Add(this.BtnSaveDeviceInfoToDB);
+            this.Name = "Form1";
+            this.Text = "Android Data Viewer";
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSMS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCalls)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvContacts;
+        private System.Windows.Forms.DataGridView dgvSMS;
+        private System.Windows.Forms.DataGridView dgvCalls;
+        private System.Windows.Forms.TextBox txtDeviceInfo;
+
+        private System.Windows.Forms.Button BtnLoadContacts;
+        private System.Windows.Forms.Button BtnSaveContactsToDB;
+        private System.Windows.Forms.Button BtnLoadSMS;
+        private System.Windows.Forms.Button BtnSaveSMSToDB;
+        private System.Windows.Forms.Button BtnLoadCalls;
+        private System.Windows.Forms.Button BtnSaveCallsToDB;
+        private System.Windows.Forms.Button BtnLoadDeviceInfo;
+        private System.Windows.Forms.Button BtnSaveDeviceInfoToDB;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace AndroidDataViewer
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=localhost;Initial Catalog=AndroidData;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Load Contacts from device
+        private void BtnLoadContacts_Click(object sender, EventArgs e)
+        {
+            string output = ExecuteAdbCommand("shell content query --uri content://contacts/phones/");
+            dgvContacts.DataSource = ParseAdbOutput(output, new[] { "display_name", "number" });
+        }
+
+        // Save Contacts to database
+        private void BtnSaveContactsToDB_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                foreach (DataGridViewRow row in dgvContacts.Rows)
+                {
+                    if (row.IsNewRow) continue;
+                    string name = row.Cells["display_name"].Value?.ToString();
+                    string number = row.Cells["number"].Value?.ToString();
+                    if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(number)) continue;  // Add a null/empty check
+                    string query = "INSERT INTO ContactsTable (Name, PhoneNumber) VALUES (@name, @number)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@name", name);
+                        cmd.Parameters.AddWithValue("@number", number);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Contacts saved to DB.");
+            }
+        }
+
+        // Load SMS
+        private void BtnLoadSMS_Click(object sender, EventArgs e)
+        {
+            string output = ExecuteAdbCommand("shell content query --uri content://sms/");
+            dgvSMS.DataSource = ParseAdbOutput(output, new[] { "address", "body", "date" });
+        }
+
+        // Save SMS
+        private void BtnSaveSMSToDB_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                foreach (DataGridViewRow row in dgvSMS.Rows)
+                {
+                    if (row.IsNewRow) continue;
+                    string senderAddress = row.Cells["address"].Value?.ToString();
+                    string message = row.Cells["body"].Value?.ToString();
+                    string timestamp = row.Cells["date"].Value?.ToString();
+                    if (string.IsNullOrEmpty(senderAddress) || string.IsNullOrEmpty(message) || string.IsNullOrEmpty(timestamp)) continue;  // Add null/empty check
+                    string query = "INSERT INTO MessagesTable (Sender, MessageContent, Timestamp) VALUES (@sender, @msg, @ts)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@sender", senderAddress);
+                        cmd.Parameters.AddWithValue("@msg", message);
+                        cmd.Parameters.AddWithValue("@ts", timestamp);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("SMS saved to DB.");
+            }
+        }
+
+        // Load Call Logs
+        private void BtnLoadCalls_Click(object sender, EventArgs e)
+        {
+            string output = ExecuteAdbCommand("shell content query --uri content://call_log/calls/");
+            dgvCalls.DataSource = ParseAdbOutput(output, new[] { "number", "type", "duration" });
+        }
+
+        // Save Call Logs
+        private void BtnSaveCallsToDB_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                foreach (DataGridViewRow row in dgvCalls.Rows)
+                {
+                    if (row.IsNewRow) continue;
+                    string number = row.Cells["number"].Value?.ToString();
+                    string type = row.Cells["type"].Value?.ToString();
+                    string duration = row.Cells["duration"].Value?.ToString();
+                    if (string.IsNullOrEmpty(number) || string.IsNullOrEmpty(type) || string.IsNullOrEmpty(duration)) continue;  // Add null/empty check
+                    string query = "INSERT INTO CallLogsTable (PhoneNumber, CallType, Duration) VALUES (@num, @type, @dur)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@num", number);
+                        cmd.Parameters.AddWithValue("@type", type);
+                        cmd.Parameters.AddWithValue("@dur", duration);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Call logs saved to DB.");
+            }
+        }
+
+        // Load Device Info
+        private void BtnLoadDeviceInfo_Click(object sender, EventArgs e)
+        {
+            string cpuInfo = ExecuteAdbCommand("shell cat /proc/cpuinfo");
+            string memInfo = ExecuteAdbCommand("shell cat /proc/meminfo");
+            txtDeviceInfo.Text = $"CPU Info:\r\n{cpuInfo}\r\n\nMemory Info:\r\n{memInfo}";
+        }
+
+        // Save Device Info
+        private void BtnSaveDeviceInfoToDB_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO DeviceInfoTable (CPUInfo, MemoryInfo) VALUES (@cpu, @mem)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    string[] parts = txtDeviceInfo.Text.Split(new[] { "\r\n\n" }, StringSplitOptions.None);
+                    cmd.Parameters.AddWithValue("@cpu", parts[0].Replace("CPU Info:\r\n", ""));
+                    cmd.Parameters.AddWithValue("@mem", parts.Length > 1 ? parts[1].Replace("Memory Info:\r\n", "") : "");
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Device info saved to DB.");
+            }
+        }
+
+        // ADB command executor
+        private string ExecuteAdbCommand(string command)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "adb",
+                Arguments = command,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            using (Process process = Process.Start(psi))
+            {
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+
+        // Parses ADB output into DataTable
+        private DataTable ParseAdbOutput(string output, string[] columns)
+        {
+            DataTable table = new DataTable();
+            foreach (string col in columns)
+                table.Columns.Add(col);
+
+            foreach (string line in output.Split(new[] { "Row " }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                DataRow row = table.NewRow();
+                foreach (string col in columns)
+                {
+                    int idx = line.IndexOf(col + "=");
+                    if (idx != -1)
+                    {
+                        int start = idx + col.Length + 1;
+                        int end = line.IndexOf(" ", start);
+                        if (end == -1) end = line.Length;
+                        row[col] = line.Substring(start, end - start).Trim();
+                    }
+                }
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+    }
+}
